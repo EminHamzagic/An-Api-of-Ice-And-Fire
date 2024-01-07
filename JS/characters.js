b3829e11461fbf465d0da71a6a1e03ec94009731
Emin Hamzagic <hamzagicemin4@gmail.com>
2024-01-07 15:43:22 +0100

    working on characters page responsivnes

--- JS/characters.js
+++ JS/characters.js
@@ -45,12 +45,14 @@ const loadCharacters = (data) => {
   infoTitle.setAttribute("id", "infoTitle");
   const infoContainer = document.createElement("div");
   infoContainer.setAttribute("id", "infoCon");
+  infoContainer.innerHTML = `<div id="loadingInfo"><div class="lds-dual-ring"></div></div>`;
   infoBox.appendChild(infoTitle);
   infoBox.appendChild(infoContainer);
   container.appendChild(infoBox);
   document.getElementById("closeBtn").addEventListener("click", () => {
     const info = document.getElementById("info");
     info.classList.toggle("active");
+    infoContainer.classList.toggle("active");
   });
 };
 
@@ -58,7 +60,6 @@ const showInfo = (char) => {
   const info = document.getElementById("info");
   info.classList.toggle("active");
   const infoCon = document.getElementById("infoCon");
-  infoCon.innerHTML = "";
   const name = document.getElementById("infoTitle");
   name.innerText = char.name ? char.name : '"' + char.aliases[0] + '"';
   Promise.all(char.books.map((book) => fetch(book).then((r) => r.json()))).then(
@@ -66,18 +67,96 @@ const showInfo = (char) => {
       Promise.all(
         char.allegiances.map((alleg) => fetch(alleg).then((r) => r.json()))
       ).then((allegiances) => {
-        const alias = document.createElement("p");
-        alias.innerText = `Aliases: ${
-          char.aliases[0] === "" ? "No data" : char.aliases.toString()
-        }`;
-
-        const bron = document.createElement("p");
-        bron.innerText = `Born: ${char.born ? "No data" : char.born}`;
-        const died = document.createElement("p");
-        died.innerText = `Died: ${char.died ? "No data" : char.died}`;
+        Promise.all(
+          char.povBooks.map((povBook) => fetch(povBook).then((r) => r.json()))
+        ).then((povBooks) => {
+          if (char.spouse)
+            fetch(char.spouse)
+              .then((res) => res.json())
+              .then((data) => {
+                createInfoData(char, books, allegiances, povBooks, data.name);
+              });
+          else createInfoData(char, books, allegiances, povBooks, "No data");
+        });
       });
     }
   );
+};
+
+const createInfoData = (char, books, allegiances, povBooks, Dspouse) => {
+  const infoCon = document.getElementById("infoCon");
+  infoCon.innerHTML = "";
+  infoCon.classList.toggle("active");
+
+  const infoP1 = document.createElement("div");
+  infoP1.setAttribute("id", "infoP1");
+  const infoP2 = document.createElement("div");
+  infoP2.setAttribute("id", "infoP2");
+  const infoP3 = document.createElement("div");
+  infoP3.setAttribute("id", "infoP3");
+
+  const alias = document.createElement("p");
+  alias.innerText = `Aliases: ${
+    char.aliases[0] === "" ? "No data" : char.aliases.toString()
+  }`;
+  const born = document.createElement("p");
+  born.innerText = `Born: ${char.born ? char.born : "No data"}`;
+  const died = document.createElement("p");
+  died.innerText = `Died: ${char.died ? char.died : "No data"}`;
+  const gender = document.createElement("p");
+  gender.innerText = `Gender: ${char.gender ? char.gender : "No data"}`;
+  const culture = document.createElement("p");
+  culture.innerText = `Culture: ${char.culture ? char.culture : "No data"}`;
+  infoP1.append(alias, born, died, gender, culture);
+
+  const father = document.createElement("p");
+  if (char.father) {
+    fetch(char.father)
+      .then((res) => res.json())
+      .then((data) => (father.innerText = `Father: ${data.name}`));
+  } else father.innerText = `Father: No data`;
+  const mother = document.createElement("p");
+  if (char.mother) {
+    fetch(char.mother)
+      .then((res) => res.json())
+      .then((data) => (mother.innerText = `Mother: ${data.name}`));
+  } else mother.innerText = `Mother: No data`;
+  const spouse = document.createElement("p");
+  spouse.innerText = `Spouse: ${Dspouse}`;
+  const titles = document.createElement("p");
+  titles.innerText = `Titles: ${
+    char.titles[0] === "" ? "None" : char.titles.toString()
+  }`;
+  const playedBy = document.createElement("p");
+  playedBy.innerText = `Played by: ${
+    char.playedBy[0] === "" ? "No data" : char.playedBy.toString()
+  }`;
+  infoP2.append(father, mother, spouse, titles, playedBy);
+
+  const pAllegiances = document.createElement("p");
+  let allegiancesArr = [];
+  if (allegiances.length > 0) {
+    for (let i = 0; i < allegiances.length; i++)
+      allegiancesArr.push(allegiances[i].name);
+  }
+  pAllegiances.innerText = `Allegiances: ${
+    allegiances.length === 0 ? "No data" : allegiancesArr.toString()
+  }`;
+  const appearsIn = document.createElement("p");
+  let booksArr = [];
+  for (let i = 0; i < books.length; i++) booksArr.push(books[i].name);
+  appearsIn.innerText = `Appears in: ${booksArr.toString()}`;
+  const povbook = document.createElement("p");
+  let povBooksArr = [];
+  for (let i = 0; i < povBooks.length; i++) povBooksArr.push(povBooks[i].name);
+  povbook.innerText = `Pov books: ${
+    povBooks.length === 0 ? "No data" : povBooksArr.toString()
+  }`;
+  const tv = document.createElement("p");
+  tv.innerText = `Tv Series Appearances: ${char.tvSeries.toString()}`;
+  infoP3.append(pAllegiances, appearsIn, povbook, tv);
+
+  infoCon.append(infoP1, infoP2, infoP3);
 };
 
 document.getElementById("prev").addEventListener("click", () => {
