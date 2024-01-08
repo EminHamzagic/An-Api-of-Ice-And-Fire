8782395ab13e63d24a89eaf390a5f40b6a11b142
Emin Hamzagic <hamzagicemin4@gmail.com>
2024-01-08 17:01:45 +0100

    wip

--- JS/characters.js
+++ JS/characters.js
@@ -1,9 +1,3 @@
-fetch(
-  "https://www.anapioficeandfire.com/api/characters?page=1&pageSize=24&isalive=true"
-)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
-
 var page = 1;
 
 const container = document.getElementById("charContainer");
@@ -171,37 +165,37 @@ document.getElementById("next").addEventListener("click", () => {
   getCharacters().then((data) => loadCharacters(data));
 });
 
-document.getElementById("filterToggle").addEventListener("click", (e) => {
-  document.getElementById("fil").classList.toggle("active");
-  e.preventDefault();
-});
+// document.getElementById("filterToggle").addEventListener("click", (e) => {
+//   document.getElementById("fil").classList.toggle("active");
+//   e.preventDefault();
+// });
 
 const male = document.getElementById("male");
 const female = document.getElementById("female");
 const alive = document.getElementById("alive");
 const dead = document.getElementById("dead");
 
-document.getElementById("apply").addEventListener("click", () => {
-  let gender = "",
-    status = "";
-  if (male.checked === true) gender = "&gender=male";
-  else if (female.checked === true) gender = "&gender=female";
-  if (alive.checked === true) status = "&isalive=true";
-  else if (dead.checked == true) status = "&isalive=false";
-  filters = gender + status;
-  getCharacters().then((data) => {
-    loadCharacters(data);
-  });
-});
+// document.getElementById("apply").addEventListener("click", () => {
+//   let gender = "",
+//     status = "";
+//   if (male.checked === true) gender = "&gender=male";
+//   else if (female.checked === true) gender = "&gender=female";
+//   if (alive.checked === true) status = "&isalive=true";
+//   else if (dead.checked == true) status = "&isalive=false";
+//   filters = gender + status;
+//   getCharacters().then((data) => {
+//     loadCharacters(data);
+//   });
+// });
 
-document.getElementById("reset").addEventListener("click", () => {
-  filters = "";
-  male.checked = false;
-  female.checked = false;
-  alive.checked = false;
-  dead.checked = false;
-  getCharacters().then((data) => loadCharacters(data));
-});
+// document.getElementById("reset").addEventListener("click", () => {
+//   filters = "";
+//   male.checked = false;
+//   female.checked = false;
+//   alive.checked = false;
+//   dead.checked = false;
+//   getCharacters().then((data) => loadCharacters(data));
+// });
 
 const loadingScreen = document.getElementById("loading");
 
