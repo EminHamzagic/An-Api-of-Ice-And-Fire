f935eadba8082470d707812b4dec7b015e4210e5
Emin Hamzagic <hamzagicemin4@gmail.com>
2024-01-13 15:20:12 +0100

    working on contact us page

--- JS/contactUs.js
+++ JS/contactUs.js
@@ -3,28 +3,59 @@ const fname = document.getElementById("fname");
 const lname = document.getElementById("lname");
 const email = document.getElementById("email");
 const msg = document.getElementById("msg");
-document.addEventListener("click", () => {
-  inputInfo[0].innerHTML =
-    inputInfo[1].innerHTML =
+
+fname.addEventListener("focus", () => {
+  inputInfo[1].innerHTML =
     inputInfo[2].innerHTML =
     inputInfo[3].innerHTML =
       "&nbsp;";
-  if (document.activeElement === fname)
-    inputInfo[0].innerHTML = "Only characters";
-  else if (document.activeElement === lname)
-    inputInfo[1].innerHTML = "Only characters";
-  else if (document.activeElement === email)
-    inputInfo[2].innerHTML = "Must be valid email adress";
-  else if (document.activeElement === msg)
-    inputInfo[3].innerHTML = "Put your message in here";
+  inputInfo[0].style.color = "white";
+  inputInfo[0].innerHTML = "a-z A-Z";
+});
+lname.addEventListener("focus", () => {
+  inputInfo[0].innerHTML =
+    inputInfo[2].innerHTML =
+    inputInfo[3].innerHTML =
+      "&nbsp;";
+  inputInfo[1].style.color = "white";
+  inputInfo[1].innerHTML = "a-z A-Z";
+});
+email.addEventListener("focus", () => {
+  inputInfo[0].innerHTML =
+    inputInfo[1].innerHTML =
+    inputInfo[3].innerHTML =
+      "&nbsp;";
+  inputInfo[2].style.color = "white";
+  inputInfo[2].innerHTML = "Ex. something@gmail.com";
+});
+msg.addEventListener("focus", () => {
+  inputInfo[0].innerHTML =
+    inputInfo[1].innerHTML =
+    inputInfo[2].innerHTML =
+      "&nbsp;";
+  inputInfo[3].style.color = "white";
+  inputInfo[3].innerHTML = "Put your message in here";
 });
 
-// if (document.activeElement === fname) console.log("radi");
+var emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+var nameRegex = /^[a-zA-Z]+$/;
+var btn = document.getElementById("contactBtn");
 
-// fname.addEventListener("focus", () => {
-//   inputInfo[0].innerHTML = "Only characters";
-// });
-
-// lname.addEventListener("focus", () => {
-//   inputInfo[1].innerHTML = "Only characters";
-// });
+btn.addEventListener("click", () => {
+  if (!emailRegex.test(email.value)) {
+    inputInfo[2].innerHTML = "Not a valid adress";
+    inputInfo[2].style.color = "red";
+  }
+  if (!nameRegex.test(fname.value)) {
+    inputInfo[0].innerHTML = "Only characters";
+    inputInfo[0].style.color = "red";
+  }
+  if (!nameRegex.test(lname.value)) {
+    inputInfo[1].innerHTML = "Only characters";
+    inputInfo[1].style.color = "red";
+  }
+  if (!msg.value) {
+    inputInfo[3].innerHTML = "Must contain a message!";
+    inputInfo[3].style.color = "red";
+  }
+});
