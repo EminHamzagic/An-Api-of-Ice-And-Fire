6a1a7e63c3e8289bc6321d58cf9a0b5bf4f9605f
Emin Hamzagic <hamzagicemin4@gmail.com>
2024-01-16 14:40:15 +0100

    Added logout button working logic

--- JS/header.js
+++ JS/header.js
@@ -13,3 +13,16 @@ window.addEventListener("scroll", () => {
   if (y > 100) header.classList.add("scroll");
   else if (y < 300) header.classList.remove("scroll");
 });
+
+if (localStorage.getItem("loggedIn")) {
+  document.getElementById("login").style.display = "none";
+  document.getElementById("logout").style.display = "flex";
+} else {
+  document.getElementById("login").style.display = "flex";
+  document.getElementById("logout").style.display = "none";
+}
+
+document.getElementById("logout").addEventListener("click", () => {
+  localStorage.removeItem("loggedIn");
+  location.href = "login.html";
+});
